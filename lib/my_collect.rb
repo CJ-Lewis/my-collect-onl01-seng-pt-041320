1c7ba8c7d5ef1965fcaa373099e4d39bfe9605f5
Christopher Lewis <cjoellewis@yahoo.com>
2020-04-26 19:22:15 +0000

    Automatically backed up by Learn

--- lib/my_collect.rb
+++ lib/my_collect.rb
@@ -1,2 +1,11 @@
-
-
+def my_collect(collection)
+  if block_given?
+    i = 0
+    collection = []
+    while i < collection.length
+    my_collect(collection) do |i|
+      i += 1
+    end
+end
+end
+end
